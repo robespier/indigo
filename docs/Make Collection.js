428a6a4e8135a380223ae9aa8ce11ea5eacf7438
Антон Домолазов <anton@domelaz.ru>
2013-06-08 17:50:55 +0400

    Сборка-утверждение

--- docs/Make Collection.js
+++ docs/Make Collection.js
@@ -4,7 +4,7 @@
 #target illustrator-13
 
 var task = 7267004; //Определяем переменные для паспорта 
-var temp =1152800; //шаблона
+var temp =1152418; //шаблона
 var roll_number = 0; //и намотки, которые задаются в окне диалога или выцепляются из базы данных
 
 //Рисуем окно диалога
@@ -16,6 +16,9 @@ var jobFolder = new Folder ('Z:\\d' + task); //Папка паспорта (ра
 var prListFolder = new Folder ('D:\\work\\print_list'); //Папка, где находятся принт-листы
 var prList = new File (prListFolder + '\\d' + task + '.csv'); //Ссылка на файл принт-листа
 var printList = []; //Массив строк из принт-листа
+
+
+
 
 var templateFolder = new Folder ('D:\\work\\template'); //Каталог шаблонов сборки
 var template = new File (templateFolder + '\\' + temp + '.ai'); //Ссылка на файл шаблона
@@ -106,15 +109,104 @@ while (line=prList.readln()) {
 	// Экспорт в PDF
 	var PDFName = jobFolder + '\\d' + task + '_'  + file_parts[0] + '_' + file_parts[1].replace ('eps', 'pdf'); //Задаем имя файла сборки
 	var ResultFilePDF = new File (PDFName);
-	myDoc.saveAs(ResultFilePDF, PDFSettings); //Сохраняем файл сборки
+	//	myDoc.saveAs(ResultFilePDF, PDFSettings); //Сохраняем файл сборки
 
 	label.remove(); // Удаляем объект label
 }
-
-//Делаем сборку-утверждение
-//Делаем сборку-внимание
 
 prList.close();
 
 //Закрываем активный документ
 myDoc.close (SaveOptions.DONOTSAVECHANGES);
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+//
+// Делаем сборку-утверждение
+//
+//
+//
+
+var template_utv = new File (templateFolder + '\\short\\' + temp + '_short.ai'); //Ссылка на файл короткого шаблона
+
+app.open (template_utv); //Открываем короткий шаблон
+
+newlayer = activeDocument.layers.add(); //Создаем слой для размещения этикеток
+newlayer.name = 'label'; //называем его именем label
+newlayer.zOrder(ZOrderMethod.SENDTOBACK); //и помещаем его в самый низ в пачке слоев документа
+
+var myDoc = app.activeDocument; //Создаем ссылку на активный документ
+myDoc.rulerOrigin = [0,0]; //Обнуляем центр координат
+
+var cuts = myDoc.layers['cut'].pathItems; //Создаем ссылку на массив высечек
+
+prList.open(); //Открываем принт-лист
+
+var i=0;
+
+while (line=prList.readln()) {
+	printList.push(line); //Считываем одну строку из print_list
+
+	var file_parts = line.split(";");
+	file_parts[0]; //Берем из строки номер этикетки
+	file_parts[1]; //Берем из строки наименование этикетки
+
+	file_name = jobFolder + '\\' + file_parts[1];
+	var labelObjectFile= new File (file_name); //Создаем ссылку на файл этикетки
+
+	var label = newlayer.placedItems.add();
+
+	label.file = labelObjectFile; //Помещаем на слой layer файл этикетки
+
+	label.position = new Array (cuts[i].position[0]+(LsizeX/2) - (label.width/2), cuts[i].position[1]-(LsizeY/2)+(label.height/2));
+
+	//  Выбор намоток
+	switch(roll_number) {
+		case 0:
+			// Ручная намотка
+			// ПРЯМОУГОЛЬНАЯ ЭТИКЕТКА
+			// 1 - квадрат, крутить не надо;
+			// 0.999 ширина меньше высоты, крутим в любую сторону на 90 градусов
+			// 1.999 ширина больше высоты, крутить не надо
+			targetCutRate = cuts[i].width/cuts[i].height;
+			labelRate = label.width/label.height;
+
+			if (((targetCutRate < 1) && (labelRate > 1)) || ((targetCutRate > 1) && (labelRate < 1)))  {
+				myStyle=myRolls['roll_1_6']; // Крутить
+			} else {
+				myStyle=myRolls['roll_4_8']; // Не крутить
+			}
+			break;
+		case 1:
+			break;
+		default:
+			alert ('No such roll');
+			break;
+	}
+
+	myStyle.applyTo(label); // Применям графический стиль к этикетке
+	i++;
+}
+
+prList.close();
+
+// Экспорт в PDF
+// d + 7267004 + _UTV.pdf
+var PDFName = jobFolder + '\\d' + task + '_UTV.pdf'; //Задаем имя файла сборки
+var ResultFilePDF = new File (PDFName);
+myDoc.saveAs(ResultFilePDF, PDFSettings); //Сохраняем файл сборки
+
+//Закрываем активный документ
+myDoc.close (SaveOptions.DONOTSAVECHANGES);
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+//
+//Делаем сборку-внимание
+
+
