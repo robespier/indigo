a80e4205680ea60b18420c9a711278fca5166959
Антон Домолазов <anton@domelaz.ru>
2013-08-28 14:12:40 +0400

    Fixed syntax errors
    С помощью jshint определил проблему - это синтаксис. Для каждого
    значения нужен свой case.
    Всё заработало, но в итоге кейсы получились эпическими - в экран
    не лезут.
    Надо с ними что-то сделать, поэтому я их отсортировал в виме и
    решил на них взглянуть с пристрастием...

--- bin/getHotFolder.jsx
+++ bin/getHotFolder.jsx
@@ -1,5 +1,3 @@
-#target Illustrator-13
-
 this.ink = new Array(7);
 
 this.ink[0] = 0; // Opaque
@@ -21,18 +19,139 @@ function toDEC(dec) { // Переводит число из двоичной с�
 
 function getHotFolder() { // Определяет hotfolder исходя из красочности задания
 	var inkDec = toDEC(this.ink);
-		switch(inkDec) {
-			case 60, 4, 8, 16, 32, 36, 40, 48, 56, 24, 20, 28, 12, 44, 52:
-				hotfolderName = 'CMYK'; // hotfolder CMYK
-					break;
-			case 124, 68, 72, 80, 96, 100, 104, 112, 120, 88, 84, 92, 76, 108, 116:
-				hotfolderName = 'CMYKW'; // hotfolder CMYKW
-					break;				
-			case 61, 5, 9, 17, 33, 37, 41, 49, 57, 25, 21, 29, 13, 45, 53, 125, 69, 73, 81, 97, 101, 105, 113, 121, 89, 85, 93, 77, 109, 117, 1, 62, 6, 10, 18, 34, 38, 42, 50, 58, 26, 22, 30, 14, 46, 54, 126, 70, 74, 82, 98, 102, 106, 114, 122, 90, 86, 94, 78, 110, 118, 2, 63, 127, 7, 11, 19, 35, 39, 43, 51, 59, 27, 23, 31, 15, 47, 55, 127, 71, 75, 83, 99, 103 ,107 ,115 ,123, 91, 87, 95, 79, 111, 119, 3:
-				hotfolderName = 'CMYKOV_White'; // hotfolder CMYKOW_White
-					break;
-		}
-return hotfolderName;
+	var hotfolderName = '';
+	switch(inkDec) {
+		case 4:
+		case 8:
+		case 12:
+		case 16:
+		case 20:
+		case 24:
+		case 28:
+		case 32:
+		case 36:
+		case 40:
+		case 44:
+		case 48:
+		case 52:
+		case 56:
+		case 60:
+			hotfolderName = 'CMYK'; // hotfolder CMYK
+			break;
+		case 68:
+		case 72:
+		case 76:
+		case 80:
+		case 84:
+		case 88:
+		case 92:
+		case 96:
+		case 100:
+		case 104:
+		case 108:
+		case 112:
+		case 116:
+		case 120:
+		case 124:
+			hotfolderName = 'CMYKW'; // hotfolder CMYKW
+			break;				
+		case 1:
+		case 2:
+		case 3:
+		case 5:
+		case 6:
+		case 7:
+		case 9:
+		case 10:
+		case 11:
+		case 13:
+		case 14:
+		case 15:
+		case 17:
+		case 18:
+		case 19:
+		case 21:
+		case 22:
+		case 23:
+		case 25:
+		case 26:
+		case 27:
+		case 29:
+		case 30:
+		case 31:
+		case 33:
+		case 34:
+		case 35:
+		case 37:
+		case 38:
+		case 39:
+		case 41:
+		case 42:
+		case 43:
+		case 45:
+		case 46:
+		case 47:
+		case 49:
+		case 50:
+		case 51:
+		case 53:
+		case 54:
+		case 55:
+		case 57:
+		case 58:
+		case 59:
+		case 61:
+		case 62:
+		case 63:
+		case 69:
+		case 70:
+		case 71:
+		case 73:
+		case 74:
+		case 75:
+		case 77:
+		case 78:
+		case 79:
+		case 81:
+		case 82:
+		case 83:
+		case 85:
+		case 86:
+		case 87:
+		case 89:
+		case 90:
+		case 91:
+		case 93:
+		case 94:
+		case 95:
+		case 97:
+		case 98:
+		case 99:
+		case 101:
+		case 102:
+		case 103:
+		case 105:
+		case 106:
+		case 107:
+		case 109:
+		case 110:
+		case 111:
+		case 113:
+		case 114:
+		case 115:
+		case 117:
+		case 118:
+		case 119:
+		case 121:
+		case 122:
+		case 123:
+		case 125:
+		case 126:
+		case 127:
+			hotfolderName = 'CMYKOV_White'; // hotfolder CMYKOW_White
+			break;
+	}
+	return hotfolderName;
 }
 
 alert(getHotFolder());
