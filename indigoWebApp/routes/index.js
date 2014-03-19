c579372a5f0e705b21f65ed376a09d9c83c56610
Антон Домолазов <anton@domelaz.ru>
2014-03-19 09:41:56 +0400

    BlankComposer из mongodb через nodejs

--- indigoWebApp/routes/index.js
+++ indigoWebApp/routes/index.js
@@ -3,37 +3,17 @@
  * GET home page.
  */
 
-exports.data = function(req,res) {
-	console.log('Here');
-	var parcel = {
-			sizeX: 69,
-			sizeY: 185,
-			diam: false,
-			designer: "Сергеев Р.",
-			manager: "Исламов Р.",
-			name: "Мед цветочный Абрико 500 г",
-			customer: "Пищехимпродукт ООО",
-			task: "3160068",
-			order: "114Ц00370",
-			lak_select: false,
-			lak_solid: true,
-			klei: false,
-			tis_hot: false,
-			tis: false,
-			kongrev: false,
-			ink_0: false,
-			ink_1: true,
-			ink_2: true,
-			ink_3: true,
-			ink_4: true,
-			ink_5: false,
-			ink_6: false
-		};
-	var json = JSON.stringify(parcel);
+var MongoClient = require('mongodb').MongoClient;
 
-	res.json( 200, [{ 
-		action: 'BlankComposer',
-		data: parcel }]
-	);
-	res.end();
+exports.data = function(req,res) {
+	MongoClient.connect('mongodb://127.0.0.1:27017/indigo', function(err, db) {
+		var jobsCollection = db.collection('indigoJobs');
+		jobsCollection.find().nextObject(function(err, parcel) {
+			res.json( 200, [{ 
+				action: 'BlankComposer',
+				data: parcel }]
+				);
+			res.end();
+		});
+	})
 };
