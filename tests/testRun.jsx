a553f532f46923f4e5e006f554fa046be1f56959
Антон Домолазов <anton@domelaz.ru>
2014-03-21 12:21:02 +0400

    Добавлен тест для BlankComposer

--- tests/testRun.jsx
+++ tests/testRun.jsx
@@ -20,4 +20,4 @@ Indigo.Tests.ts.init();
 Indigo.Tests.ts.runAllTests();
 
 // ...или запустить один тест:
-//Indigo.Tests.ts.execute(new Indigo.Tests.testJsonBroker());
+//Indigo.Tests.ts.execute(new Indigo.Tests.testBlankComposer());
