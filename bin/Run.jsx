9fdcde562400e203d100e03dbb63c00d5520a9c0
robespier <robespier@rambler.ru>
2014-03-23 12:46:34 +0400

    my test commit

--- bin/Run.jsx
+++ bin/Run.jsx
@@ -1,4 +1,4 @@
-#include ../include/indigo.jsxinc
+﻿#include ../include/indigo.jsxinc
 
 // Тут, ясное дело, надо прописать реальный путь к webaccesslib.dll 
 var webaccesslib = new ExternalObject('lib:' + Indigo.config.webaccesslib);
@@ -7,3 +7,5 @@ var c = new Indigo.Controller();
 // АЛГА!!!
 c.run();
 c.cleanup();
+
+// Мой коммент!
