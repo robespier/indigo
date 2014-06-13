b17e2acaed48f412e0663408b975dbd3c47aaf6e
Антон Домолазов <anton@domelaz.ru>
2014-06-13 22:27:58 +0400

    В контроллер Job скопирован функционал Blank
    * говнокод? Да! Он самый!
    * но мы чуть позже от него избавимся
    * и клики по намоткам теперь работают

--- indigoWebApp/public/js/indigo.js
+++ indigoWebApp/public/js/indigo.js
@@ -37,7 +37,44 @@
 		};
 	}]);
 
-	app.controller('JobBlank', ['$scope', 'Job', function($scope, Job) {} ]);
+	app.controller('JobBlank', ['$scope', 'Job', 'Blank', function($scope, Job, Blank) {
+		/**
+		 * Загрузка данных бланка откуда-нибудь
+		 * _id на шару, нода сейчас отвечает единственным набором данных
+		 */
+		$scope.order = Blank.get({_id:'example'});
+
+		/**
+		 * Get image of roll direction
+		 *
+		 * @param {string} direction Predefined direction
+		 * @returns {string} img Icon relative filename
+		 */
+		$scope.getRollImage = function(direction) {
+			var dirMap = {
+				head_mashine: 'roll_1_6',
+				foot_mashine: 'roll_2_5',
+				foot_forward: 'roll_3_7',
+				head_forward: 'roll_4_8',
+			};
+			var img = 'roll_undefinded';
+
+			if (dirMap[direction]) {
+				img = dirMap[direction];
+			}
+			return img + '.png';
+		};
+
+		/**
+		 * Set roll image from click on rolls radio group
+		 *
+		 * @param {string} direction Predefined direction
+		 */
+		$scope.setRollImage = function(direction) {
+			this.order.roll_dir = direction;
+		};
+
+	} ]);
 	/**
 	 * Валидация формы: ожидается положительный float 
 	 *
