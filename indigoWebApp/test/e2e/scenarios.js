195b290dd1ddad7c00b7ff24134256efdecc50f1
Антон Домолазов <anton@domelaz.ru>
2014-06-14 09:01:44 +0400

    Добавлен chai, assertion library для mocha
    * по русски не знаю как сказать, что такое assertion;
    * нам она потребуется, чтобы писать тесты на человеческом языке;
    * для моки есть и другие assertion libraries, chai одна из них:
      @see http://visionmedia.github.io/mocha/#assertions

--- indigoWebApp/test/e2e/scenarios.js
+++ indigoWebApp/test/e2e/scenarios.js
@@ -1,7 +1,7 @@
-//var chai = require('chai');
-//var chaiAsPromised = require('chai-as-promised');
-//chai.use(chaiAsPromised);
-//var expect = chai.expect;
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
 
 describe('upload illustrator job', function() {
 	beforeEach(function() {
