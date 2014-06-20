ff9a48bad72fb91216d2b154f7e3c2c51c929af6
Антон Домолазов <anton@domelaz.ru>
2014-06-20 15:05:36 +0400

    Тесты для отображения направления намотки

--- indigoWebApp/test/e2e/scenarios.js
+++ indigoWebApp/test/e2e/scenarios.js
@@ -4,7 +4,6 @@ chai.use(chaiAsPromised);
 var expect = chai.expect;
 
 describe('upload illustrator job', function() {
-
 	var labels, sbm;
 
 	beforeEach(function() {
@@ -80,5 +79,39 @@ describe('select hotfolder', function() {
 		Op.click();
 		Op.click();
 		expect(hotFolder.getText()).to.eventually.equal('CMYK');
+	});
+});
+
+describe('display roll image', function() {
+	var head_mashine, foot_mashine, foot_forward, head_forward,
+		roll_image;
+
+	beforeEach(function() {
+		browser.get('index.html');
+		head_mashine = element(by.id('head_mashine'));
+		foot_mashine = element(by.id('foot_mashine'));
+		foot_forward = element(by.id('foot_forward'));
+		head_forward = element(by.id('head_forward'));
+		roll_image   = element(by.id('roll_image'));
+	});
+
+	it('should be "head_mashine" image', function() {
+		head_mashine.click();
+		expect(roll_image.getAttribute('src')).to.eventually.string('images/roll_1_6.png');
+	});
+
+	it('should be "foot_mashine" image', function() {
+		foot_mashine.click();
+		expect(roll_image.getAttribute('src')).to.eventually.string('images/roll_2_5.png');
+	});
+
+	it('should be "foot_forward" image', function() {
+		foot_forward.click();
+		expect(roll_image.getAttribute('src')).to.eventually.string('images/roll_3_7.png');
+	});
+
+	it('should be "head_forward" image', function() {
+		head_forward.click();
+		expect(roll_image.getAttribute('src')).to.eventually.string('images/roll_4_8.png');
 	});
 });
