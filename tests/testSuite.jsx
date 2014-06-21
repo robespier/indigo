b0d2983c12d91564fd8f9e21d2e9f5497f7f2178
Антон Домолазов <anton@domelaz.ru>
2014-06-21 10:40:31 +0400

    Изменение подключения webaccesslib в тестах estk
    * копипста из Run.jsx, ничего более;

--- tests/testSuite.jsx
+++ tests/testSuite.jsx
@@ -1,4 +1,15 @@
 var Indigo = Indigo || {};
+
+var webaccesslib;
+// Будет использован первый найденный webaccesslib из конфига indigo
+for (var path_index in Indigo.config.webaccesslib) {
+	var lib_file = new File(Indigo.config.webaccesslib[path_index]);
+	if (lib_file.exists) {
+		webaccesslib = new ExternalObject('lib:' + lib_file.fullName);
+		break;
+	}
+}
+
 /**
  * Тесты будут жить в своём углу
  * @namespace
