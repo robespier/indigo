83e672e45ea42474aa02e4dbb92445d9fb285f26
robespier <robespier@rambler.ru>
2014-06-24 00:52:54 +0400

    Затестил, все работает!
    Даже изменения в GetPDFName прокатили. Удивительно! Этот "пробел" можно
    считать закрытым.

--- indigoWebApp/public/js/indigo.js
+++ indigoWebApp/public/js/indigo.js
@@ -58,8 +58,8 @@
 			hot_folder: 'CMYK',
 			actions: [
 				{ name: 'AssemblyImposer', process: true },
-				{ name: 'matching', process: false },
-				{ name: 'achtung', process: false },
+				{ name: 'MatchingImposer', process: true },
+				{ name: 'AchtungImposer', process: true },
 			],
 		};
 		
