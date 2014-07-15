045324e096af4aedf6fe59c3dcf2356dc6c5ec2c
Антон Домолазов <anton@domelaz.ru>
2014-07-15 09:07:12 +0400

    estk фиксы тестов
    * учтены изменения в предыдущих пяти коммитах;
    * тесты копируются на сервер и доступны агентам;
    * добавлены .gitignore в пустые хотфолдеры. git не коммитит пустые
      папки, структура каталогов не сохраняется и тесты падают;

--- misc/codesync.sh
+++ misc/codesync.sh
@@ -2,3 +2,5 @@
 
 scp include/indigo.jsxinc root@centos-10:/dustbox/indigo/app/include/
 scp bin/Run.jsx root@centos-10:/dustbox/indigo/app/bin/
+scp bin/tests.js root@centos-10:/dustbox/indigo/app/bin/
+rsync -r tests/ root@centos-10:/dustbox/indigo/app/tests/
