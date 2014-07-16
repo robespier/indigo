4120d51a1290d55d19fd2b99c1e8d2eb61b83a9d
Антон Домолазов <anton@domelaz.ru>
2014-07-16 15:43:27 +0400

    angular все html файлы убраны в отдельную папку
    * все, кроме index.html;

--- indigoWebApp/public/js/indigo.js
+++ indigoWebApp/public/js/indigo.js
@@ -1,40 +1,38 @@
-(function() {
-	var app = angular.module('indigo', [
-		'ngRoute',
-		'indigoControllers',
-		'indigoDatasource',
-	]);
+var app = angular.module('indigo', [
+	'ngRoute',
+	'indigoControllers',
+	'indigoDatasource',
+]);
 
-	app.config(['$routeProvider', 
-		function($routeProvider) {
-			$routeProvider.
-				when('/jobs', {
-					templateUrl: 'jobs.html',
-				}).
-				when('/addJob', {
-					templateUrl: 'jobPush.html',
-				}).
-				when('/templates', {
-					templateUrl: 'templates.html',
-				}).
-				otherwise({
-					redirectTo: '/dashboard.html'
+app.config(['$routeProvider', 
+	function($routeProvider) {
+		$routeProvider.
+			when('/jobs', {
+				templateUrl: 'paritals/jobs.list.html',
+			}).
+			when('/addJob', {
+				templateUrl: 'paritals/job.edit.html',
+			}).
+			when('/templates', {
+				templateUrl: 'paritals/templates.html',
+			}).
+			otherwise({
+				redirectTo: 'paritals/dashboard.html'
+			});
+	}
+]);
+
+app.factory('socket', function($rootScope) {
+	/* global io */
+	var socket = io.connect();
+	return {
+		on: function(eventName, callback) {
+			socket.on(eventName, function() {
+				var args = arguments;
+				$rootScope.$apply(function() {
+					callback.apply(socket, args);
 				});
+			});
 		}
-	]);
-
-	app.factory('socket', function($rootScope) {
-		/* global io */
-		var socket = io.connect();
-		return {
-			on: function(eventName, callback) {
-				socket.on(eventName, function() {
-					var args = arguments;
-					$rootScope.$apply(function() {
-						callback.apply(socket, args);
-					});
-				});
-			}
-		};
-	});
-})();
+	};
+});
